6d1f0699b8f5172d18654eabcb9add627ea47fa5
oğuz kaan baydere <okbaydere@gmail.com>
2021-08-31 09:10:22 +0300

    ..

--- Görev2/website/models.py
+++ Görev2/website/models.py
@@ -8,7 +8,7 @@ from . import db
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(10000))
-    date = db.Column(db.DateTime(), default=datetime.now())
+    date = db.Column(db.DateTime(), default=datetime.now)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     task_point = db.Column(db.Integer)
 
