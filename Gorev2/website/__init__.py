f7ce27eea6982916b8fc0bb663fa8e047562ee2c
oğuz kaan baydere <okbaydere@gmail.com>
2021-09-11 16:46:58 +0300

    SQLite database'i yerine MYSQL database'ine geçildi

--- Gorev2/website/__init__.py
+++ Gorev2/website/__init__.py
@@ -1,10 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from os import path
 from flask_login import LoginManager
 
 db = SQLAlchemy()
-DB_NAME = "database.db"
 
 
 def create_app():
@@ -22,8 +20,6 @@ def create_app():
 
     from .models import User, Task
 
-    create_database(app)
-
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
@@ -33,9 +29,3 @@ def create_app():
         return User.query.get(int(id))
 
     return app
-
-
-def create_database(app):
-    if not path.exists('website/' + DB_NAME):
-        db.create_all(app=app)
-        print('Created Database!')
