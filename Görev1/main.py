e9b6d15d7660bfe698a49c43aae021f4aca358a4
okbaydere <okbaydere@gmail.com>
2023-07-27 10:27:59 +0300

    Update main.py

--- Görev1/main.py
+++ Görev1/main.py
@@ -3,36 +3,35 @@ from database import *
 
 def login_menu():
     print("""
-    Bir seçim yapınız
-    1- Giriş Yap
-    2- Üye Ol
-    3- Çıkış yap
+    Choose an option:
+    1- Log In
+    2- Sign Up
+    3- Exit
     """)
 
 
 def task_menu():
     print("""
-    1-Görev Olustur ve puan Ver
-    2-Tarihe göre listele
-    3-Puana göre listele
-    4-Görevlerin ID'sini gör
-    5-Görev sil
-    6-Ana menüye dön
+    1-Create Task and Assign Points
+    2-List by Date
+    3-List by Points
+    4-View Task IDs
+    5-Delete Task
+    6-Back to Main Menu
     """)
 
 
 def add_data():
-    username = input("Kullanıcı adı oluştur:")
-    password = input("Şifre oluştur:")
+    username = input("Create a username:")
+    password = input("Create a password:")
     add_user_data(username, password)
 
 
 def task_list(select_task):
     if select_task == 1:
-        task = input("Oluşturmak istediğiniz görevi giriniz:")
-        task_point = int(input("Oluşturduğunuz görevin puanını giriniz:"))
+        task = input("Enter the task you want to create:")
+        task_point = int(input("Enter the points for the task you created:"))
         add_task_data(task, task_point, username)
-
 
     elif select_task == 2:
         list_by_date(control[0])
@@ -41,40 +40,38 @@ def task_list(select_task):
     elif select_task == 4:
         show_id(control[0])
     elif select_task == 5:
-        delete = int(input("Silmek istediğiniz görevin ID'sini giriniz:"))
+        delete = int(input("Enter the ID of the task you want to delete:"))
         delete_task(delete)
 
 
 while True:
-
     login_menu()
     try:
-        select = int(input("Bir seçim yapınız:"))
+        select = int(input("Choose an option:"))
         if select == 1:
-            username = input("Kullanıcı adı:")
-            password = input("Şifre:")
+            username = input("Username:")
+            password = input("Password:")
             control = is_user_exist(username)
             if control is None:
-                print("Böyle bir kullanıcı yok")
+                print("User does not exist")
                 continue
             if password == control[2]:
                 while True:
                     task_menu()
                     try:
-                        select_task = int(input("Bir seçim yapınız:"))
+                        select_task = int(input("Choose an option:"))
                         task_list(select_task)
                         if select_task == 6:
                             break
                     except ValueError:
-                        print("Lütfen rakam giriniz")
+                        print("Please enter a number")
             elif password != control[2]:
-                print("Şifre Hatalı")
+                print("Incorrect Password")
 
         if select == 2:
             add_data()
         if select == 3:
             break
 
-
     except ValueError:
-        print("Lütfen rakam girin")
+        print("Please enter a number")
